62efb96e2d8a1d556cc4d2b1dc455b95edea3f54
gangan <44604921+Shimpei-GANGAN@users.noreply.github.com>
2019-04-26 02:29:07 +0900

    Update minecraft_1.py

--- minecraft_1.py
+++ minecraft_1.py
@@ -2,6 +2,6 @@
 import mcpi.minecraft as mine
 import mcpi.block as block
 
-mc = mine.Minecraft.create()
-x, y, z = mc.player.getPos()
-mc.setBlock( x, y+3, z+5, block.WOOD_PLANKS.id)
+mc = mine.Minecraft.create()  #  マインクラフトに接続
+x, y, z = mc.player.getPos()  #  プレイヤーの位置を取得
+mc.setBlock( x, y+3, z+5, block.WOOD_PLANKS.id)  #  木のブロックを設置する
