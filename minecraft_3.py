c0526fee9888b8215fc83baf424cfd3f88fd5b54
gangan <44604921+Shimpei-GANGAN@users.noreply.github.com>
2019-04-26 02:33:46 +0900

    Update minecraft_3.py

--- minecraft_3.py
+++ minecraft_3.py
@@ -2,13 +2,17 @@
 import mcpi.minecraft as mine
 import mcpi.block as block
 
+#  空間を初期化
 mc = mine.Minecraft.create()
 mc.setBlocks( -10, 0, -10, 10, 99, 10, block.AIR.id)
 mc.setBlocks( -10, -1, -10, 10, -99, 10, block.GRASS.id)
 x = 0
 y = 0
 z = 0
+#  プレイヤーを移動
 mc.player.setPos(x, y, z-9)
 
+#  石ブロックをセット
 mc.setBlocks(x-3, y+3, z-3, x+3, y+3, z+3, block.STONE.id)
+#  流体の水ブロックをセット
 mc.setBlocks(x, y+6, z, block.WATER_FLOWING.id)
